a8abf326e274acac1d7cbfa0b8095e5213cf8eeb
AScodersWH <895052253@qq.com>
2018-09-12 11:27:47 +0800

    add mockbean product repository

--- src/test/java/com/thoughtworks/test/tomwang/controller/ProductsApiTest.java
+++ src/test/java/com/thoughtworks/test/tomwang/controller/ProductsApiTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +23,9 @@ public class ProductsApiTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private ProductRepository productRepository;
 
     @Test
     public void should_create_successfully() throws Exception {
