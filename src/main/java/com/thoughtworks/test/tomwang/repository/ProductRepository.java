167e68d934012225f6b9470c37840783f819b7d1
AScodersWH <895052253@qq.com>
2018-09-12 14:12:46 +0800

    add delete product api

--- src/main/java/com/thoughtworks/test/tomwang/repository/ProductRepository.java
+++ src/main/java/com/thoughtworks/test/tomwang/repository/ProductRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product,Integer> {
     List<Product> findAll();
     Product findOneByName(String name);
+    String deleteOneByName(String name);
 }
